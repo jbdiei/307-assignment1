2c0bd7184672b6c6a2c3bd2e8bac0ab8133f0a5e
jbdiei <jeanbrandondiei@gmail.com>
2024-10-15 23:58:51 -0700

    adding cors

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"; 
 
 const app = express();
 const port =8000;
@@ -63,6 +64,8 @@ const findUserByNameAndJob = (name, job) =>{
         (user)=>(user["name"]=== name)&& (user["job"]=== job))
 
 }
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
